d674e43a45c87ee544940765d9afa1a6cc1cdde3
recad <dacer-133@hotmail.com>
2019-03-27 23:58:26 -0500

    se arregla problema con diagonales

--- Gemfile
+++ Gemfile
@@ -7,3 +7,13 @@ gem 'rspec'
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
+7
+0
+2
+6
+4
+3
+5
+1
+9
+8
--- algoritmoGenetico.rb
+++ algoritmoGenetico.rb
@@ -21,6 +21,7 @@
 
 require 'rubygems'
 require 'bundler/setup' 
+require 'matrix'
 
 #Definicion de la clase cromosoma
 class Cromosoma < Array 
@@ -104,13 +105,14 @@ class Cromosoma < Array
   #|   | X |  
    def calcularDiagonalPrincipal (indice, valor)
 	 valores = Array.new
+	 indices = Array.new
 	 
 	 indicefor = indice
 	 valorfor  = valor
 		
 		##Se calculan los valores diagonales hacia adelante
 		while (indicefor <= (@tamano-1) && valorfor <= (@tamano-1))  do
-			
+			indices << indicefor
 			valores << valorfor
 			indicefor +=1
 			valorfor  +=1
@@ -118,20 +120,25 @@ class Cromosoma < Array
 		end
 		
 		##Se calculan los valores diagonales hacia atras
-		
+		indicesnew = Array.new
 		valoresnew = Array.new
 		indicefor = indice-1
 		valorfor  = valor-1
 		while (indicefor >= 0 && valorfor >= 0)  do
-			
+			indicesnew << indicefor
 			valoresnew << valorfor
 			indicefor -=1
 			valorfor  -=1
 			
 		end
 		
-		puts valoresnew.reverse + valores
-		return  valoresnew.reverse + valores
+		matriz = Matrix[indicesnew.reverse + indices,valoresnew.reverse + valores]
+		#puts matriz[0]
+		#puts "values"
+		
+		# matris [filas (0 para indices- 1 para valores) , columnas son valores(valor inicial...valor final)]
+		#puts matriz[0,5]
+		return matriz
   
   
   end
@@ -140,7 +147,7 @@ class Cromosoma < Array
   #|   | X | 
   #| X |   |  
    def calcularDiagonalSecundaria (indice, valor)
-	 
+	 indices = Array.new
 	 valores = Array.new
 	 
 	 indicefor = indice
@@ -149,6 +156,7 @@ class Cromosoma < Array
 		##Se calculan los valores diagonales hacia adelante
 		while (indicefor <= (@tamano-1) && valorfor >= 0)  do
 			
+			indices << indicefor
 			valores << valorfor
 			indicefor +=1
 			valorfor  -=1
@@ -157,21 +165,26 @@ class Cromosoma < Array
 		
 		##Se calculan los valores diagonales hacia atras
 		valoresnew = Array.new
+		indicesnew = Array.new
 		indicefor = indice-1
 		valorfor  = valor+1
 		while (indicefor >= 0 && valorfor <= (@tamano-1))  do
 			
+			indicesnew << indicefor
 			valoresnew << valorfor
 			indicefor -=1
 			valorfor  +=1
 			
 		end
-		
-		return valoresnew.reverse + valores
+		matriz = Matrix[indicesnew.reverse + indices, valoresnew.reverse + valores]
+		#puts matriz[0]
+		#puts "values"
+		#puts matriz[1]
+		return matriz
   end
   
   
-  
+=begin  
   ##Funcion calcularActitud
   #retorna la actitud de el cromosoma detectando sus ataques diagonales
   def calcularActitud
@@ -183,7 +196,7 @@ class Cromosoma < Array
 	end
     
   end
- 
+=end 
 end
 
 
@@ -221,7 +234,8 @@ class Genetic < Array
 	#realizar mutación
 		self.each_with_index do |item, index|
 			puts "se muta cromosoma: #{index}"
-			item.Mutar(3)
+			#item.Mutar(3)
+			Actitud(item)
 		end
 	##Se evaluan los cromosomas y pasa el (o los) de mayor aptitud
 	##estos pasan al siguiente generacion
@@ -235,12 +249,15 @@ class Genetic < Array
 
 	def Actitud(cromosoma)
 		
+		actitud=0
+		
 		cromosoma.each_with_index do |item, index|
+		
+		puts cromosoma.calcularDiagonalPrincipal(index,item)
 			
-			
-			
-			
-			
+		#puts "se pega con diagonales secundarias"
+		#puts  cromosoma-cromosoma.calcularDiagonalSecundaria(index,item)	
+				
 		end
 	
 	
@@ -254,7 +271,9 @@ end
 cormo = Cromosoma.new(8)
 puts "primaria"
 cormo.calcularDiagonalPrincipal(6,4)
+
 puts "secundaria"
 cormo.calcularDiagonalSecundaria(6,4)
+
 #hi = Genetic.new(4,10)
 #hi.Ejecutar
